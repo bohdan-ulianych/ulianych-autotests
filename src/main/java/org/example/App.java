6296ac2803972cadad19745fa1aa6b2a26a62734
Bohdan <antoshkayepta@gmail.com>
2021-06-15 16:49:19 +0300

    work3_JSExecutor_Actions_implemented

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        WebDriver driver = null;
+        Tests test1 = new Tests(driver, "andersenTEST", "test1");
+        test1.logIneJSExecutor();
+        test1.openNewTabAndLogin();
     }
 }
